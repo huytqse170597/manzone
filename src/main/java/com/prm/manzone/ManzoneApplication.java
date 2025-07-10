4711c0b7579f113a46b4e3745131bab5114f0b2f
Tran Quang Huy (K17 HCM) <91615324+huytqse170597@users.noreply.github.com>
2025-07-10 19:52:05 +0700

    init

---
+++ src/main/java/com/prm/manzone/ManzoneApplication.java
@@ -0,0 +1,13 @@
+package com.prm.manzone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManzoneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManzoneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/prm/manzone/ManzoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prm.manzone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManzoneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
