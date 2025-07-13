1cf54f94260252809c3edf3abd8d19700a4994f2
lion3993vn <lion3993vn@gmail.com>
2025-07-13 21:38:56 +0700

    add: crud product (finished)

--- src/main/java/com/prm/manzone/controller/ProductController.java
+++ src/main/java/com/prm/manzone/controller/ProductController.java
@@ -1,13 +1,86 @@
 package com.prm.manzone.controller;
 
+import com.prm.manzone.payload.ApiResponse;
+import com.prm.manzone.payload.category.CategoryResponse;
+import com.prm.manzone.payload.product.ProductRequest;
+import com.prm.manzone.payload.product.ProductResponse;
+import com.prm.manzone.service.IProductService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/products")
 @RequiredArgsConstructor
 @Tag(name = "Product", description = "API for managing products")
 public class ProductController {
+
+    private final IProductService productService;
+
+    @Operation(summary = "Get all Products")
+    @GetMapping
+    public ResponseEntity<ApiResponse<Page<ProductResponse>>> getAllProducts(
+            @RequestParam(required = false) Integer page,
+            @RequestParam (required = false) Integer size,
+            @RequestParam(required = false) String search) {
+        Page<ProductResponse> response = productService.getAllProducts(page, size, search);
+        return ResponseEntity.ok(
+                ApiResponse.<Page<ProductResponse>>builder()
+                        .success(true)
+                        .message("Products retrieved successfully")
+                        .data(response)
+                        .build());
+    }
+
+    @Operation(summary = "Delete a product")
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ApiResponse<Void>> deleteProduct(@PathVariable int id) {
+        productService.deleteProduct(id);
+        return ResponseEntity.ok(
+                ApiResponse.<Void>builder()
+                        .success(true)
+                        .message("Product deleted successfully")
+                        .build());
+    }
+
+    @Operation(summary = "Create a new product")
+    @PostMapping
+    public ResponseEntity<ApiResponse<ProductResponse>> createProduct(@RequestBody @Valid ProductRequest request) {
+        ProductResponse response = productService.createProduct(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(
+                ApiResponse.<ProductResponse>builder()
+                        .success(true)
+                        .message("Product created successfully")
+                        .data(response)
+                        .build());
+    }
+
+    @Operation(summary = "Update an existing product")
+    @PutMapping("/{id}")
+    public ResponseEntity<ApiResponse<ProductResponse>> updateProduct(@PathVariable int id, @RequestBody @Valid ProductRequest request) {
+        ProductResponse response = productService.updateProduct(id, request);
+        return ResponseEntity.ok(
+                ApiResponse.<ProductResponse>builder()
+                        .success(true)
+                        .message("Product updated successfully")
+                        .data(response)
+                        .build());
+    }
+
+    @Operation(summary = "Get a product by ID")
+    @GetMapping("/{id}")
+    public ResponseEntity<ApiResponse<ProductResponse>> getProductById(@PathVariable int id) {
+        ProductResponse response = productService.getProductById(id);
+        return ResponseEntity.ok(
+                ApiResponse.<ProductResponse>builder()
+                        .success(true)
+                        .message("Product retrieved successfully")
+                        .data(response)
+                        .build());
+    }
 }
--- src/main/java/com/prm/manzone/service/impl/ProductServiceImpl.java
+++ src/main/java/com/prm/manzone/service/impl/ProductServiceImpl.java
@@ -1,10 +1,14 @@
 package com.prm.manzone.service.impl;
 
+import com.prm.manzone.entities.Category;
 import com.prm.manzone.entities.Product;
 import com.prm.manzone.mapper.ProductMapper;
+import com.prm.manzone.payload.product.ProductRequest;
 import com.prm.manzone.payload.product.ProductResponse;
+import com.prm.manzone.repository.CategoryRepository;
 import com.prm.manzone.repository.ProductRepository;
 import com.prm.manzone.service.IProductService;
+import com.prm.manzone.specification.ProductSpecification;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -16,6 +20,7 @@ import org.springframework.stereotype.Service;
 public class ProductServiceImpl implements IProductService {
 
     private final ProductRepository productRepository;
+    private final CategoryRepository categoryRepository;
 
     @Override
     public Page<ProductResponse> getAllProducts(Integer page, Integer size, String search) {
@@ -28,5 +33,49 @@ public class ProductServiceImpl implements IProductService {
         }
 
         return productRepository.findAll(spec, pageable).map(ProductMapper::mapToResponse);
+    }
+
+    @Override
+    public void deleteProduct(int id) {
+        Product product = productRepository.findByIdAndDeletedFalse(id)
+                .orElseThrow(() -> new RuntimeException("Product not found"));
+        product.setDeleted(true);
+        productRepository.save(product);
+    }
+
+    @Override
+    public ProductResponse createProduct(ProductRequest request) {
+        Category category = categoryRepository.findByIdAndDeletedFalse(request.getCategoryId())
+                .orElseThrow(() -> new RuntimeException("Category not found"));
+        Product product = new Product();
+        product.setName(request.getName());
+        product.setDescription(request.getDescription());
+        product.setPrice(request.getPrice());
+        product.setImageUrls(request.getImageUrls());
+        product.setCategory(category);
+        Product saved = productRepository.save(product);
+        return ProductMapper.mapToResponse(saved);
+    }
+
+    @Override
+    public ProductResponse updateProduct(int id, ProductRequest request) {
+        Product product = productRepository.findByIdAndDeletedFalse(id)
+                .orElseThrow(() -> new RuntimeException("Product not found"));
+        Category category = categoryRepository.findByIdAndDeletedFalse(request.getCategoryId())
+                .orElseThrow(() -> new RuntimeException("Category not found"));
+        product.setName(request.getName());
+        product.setDescription(request.getDescription());
+        product.setPrice(request.getPrice());
+        product.setImageUrls(request.getImageUrls());
+        product.setCategory(category);
+        Product saved = productRepository.save(product);
+        return ProductMapper.mapToResponse(saved);
+    }
+
+    @Override
+    public ProductResponse getProductById(int id) {
+        Product product = productRepository.findByIdAndDeletedFalse(id)
+                .orElseThrow(() -> new RuntimeException("Product not found"));
+        return ProductMapper.mapToResponse(product);
     }
 }
